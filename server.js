21c549af2e18ef24a3986d737bb992671d72c3ac
Elzo475 <youssefslim100@gmail.com>
2026-03-28 08:17:47 +0100

    login fix 2

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.use(session({
     resave: false,
     saveUninitialized: false,
     cookie: {
-        secure: process.env.NODE_ENV === 'production',
+        secure: true,
         httpOnly: true,
         sameSite: 'lax',       // 🔥 VERY IMPORTANT
         maxAge: 24 * 60 * 60 * 1000 // 1 day
